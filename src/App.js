ae11b905b90a0c3807eb9dda40bbd22634138b44
Joshua Crocker <joshua+code@crocker.io>
2021-01-05 20:43:23 +0000

    Add Prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import Board from './Board';
 class App extends React.Component {
-  constructor(props) {    
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isWhiteTurn: true,
-    };
-  }
+        this.state = {
+            isWhiteTurn: true,
+        };
+    }
 
-  render() {
-    return (
-      <div className="clearfix">
-        <div className="float-left">
-          <Board />
-        </div>
+    render() {
+        return (
+            <div className="clearfix">
+                <div className="float-left">
+                    <Board />
+                </div>
 
-        <div className="float-right">
-          <p>Current Player: {this.state.isWhiteTurn ? 'White' : 'Black'}</p>
-        </div>
-      </div>
-    );
-  }
+                <div className="float-right">
+                    <p>
+                        Current Player:{' '}
+                        {this.state.isWhiteTurn ? 'White' : 'Black'}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Piece.js
+++ src/Piece.js
@@ -3,21 +3,21 @@ import Constants from './Constants';
 
 class Piece extends React.Component {
     getPiece(pieceCode) {
-        switch(pieceCode) {
-        case 'p':
-            return 'pawn';
-        case 'r':
-            return 'rook';
-        case 'n':
-            return 'knight';
-        case 'b':
-            return 'bishop';
-        case 'q':
-            return 'queen';
-        case 'k':
-            return 'king';
-        default:
-            return 'pawn';
+        switch (pieceCode) {
+            case 'p':
+                return 'pawn';
+            case 'r':
+                return 'rook';
+            case 'n':
+                return 'knight';
+            case 'b':
+                return 'bishop';
+            case 'q':
+                return 'queen';
+            case 'k':
+                return 'king';
+            default:
+                return 'pawn';
         }
     }
 
@@ -30,9 +30,16 @@ class Piece extends React.Component {
 
     render() {
         return this.props.code != null ? (
-        <img src={this.getUrl()} alt={this.props.code} width={Constants.CELL_SIZE} height={Constants.CELL_SIZE} />
-        ) : '';
-}
+            <img
+                src={this.getUrl()}
+                alt={this.props.code}
+                width={Constants.CELL_SIZE}
+                height={Constants.CELL_SIZE}
+            />
+        ) : (
+            ''
+        );
+    }
 }
 
 export default Piece;
